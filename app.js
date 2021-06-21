4525dab3865e5e001f54022054b91906ce024d97
Akuya Ekorot <hello@akuya.co.ke>
2021-06-21 07:50:05 +0300

    add notion api to capture leads from form

--- app.js
+++ app.js
@@ -1,59 +1,69 @@
-const express = require('express');
-const mailchimp = require('@mailchimp/mailchimp_marketing');
+require("dotenv").config();
+
+const express = require("express");
+const { Client } = require("@notionhq/client");
 
 const app = express();
 app.use(express.static("public"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-function catchEm(promise) {
-    return promise.then(data => [null, data])
-      .catch(err => [err]);
-}
-
-// mailchimp constants
-const listId = "0c8e07ea7c";
-const apiKey = "ccdaebf66cd13f0176a4ab2fa41eaa7b";
-const mailchimpServer = "us6";
-
-mailchimp.setConfig({
-    apiKey: apiKey,
-    server: mailchimpServer,
+const notion = new Client({
+  auth: process.env.NOTION_KEY,
 });
 
+function catchEm(promise) {
+  return promise.then((data) => [null, data]).catch((err) => [err]);
+}
+
 app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/index.html`);
+  res.sendFile(`${__dirname}/index.html`);
 });
 
 app.post("/", (req, res) => {
-    const firstName = req.body.fName;
-    const lastName = req.body.lName;
-    const email = req.body.email;
-    const phone = req.body.phone;
-    const message = req.body.message;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const message = req.body.message;
 
-    const memberObject = {
-        email_address: `${email}`,
-        status: "subscribed",
-        merge_fields: {
-            FNAME: `${firstName}`,
-            LNAME: `${lastName}`,
-            PHONE: `${phone}`,
-            MESSAGE: `${message}`
-        }
+  const contact = {
+    parent: {
+      database_id: process.env.NOTION_DATABASE_ID,
+    },
+    properties: {
+      title: {
+        title: [
+          { type: "text", text: { content: `${firstName} ${lastName}` } },
+        ],
+      },
+      firstName: {
+        rich_text: [{ type: "text", text: { content: `${firstName}` } }],
+      },
+      lastName: {
+        rich_text: [{ type: "text", text: { content: `${lastName}` } }],
+      },
+      phone: { phone_number: phone },
+      email: { email: email },
+      message: {
+        rich_text: [{ type: "text", text: { content: `${message}` } }],
+      },
+      tags: {
+        multi_select: [{ name: "Lead" }],
+      },
+    },
+  };
+
+  const addMember = async (contact) => {
+    const [err, data] = await catchEm(notion.pages.create(contact));
+
+    if (err) {
+      res.sendFile(`${__dirname}/fail.html`);
+    } else {
+      res.sendFile(`${__dirname}/success.html`);
     }
+  };
 
-    const addMember = async () => {
-        const [err, data] = await catchEm(mailchimp.lists.addListMember(listId, memberObject));
-        
-        if (err) {
-            res.sendFile(`${__dirname}/fail.html`);
-        } else {
-            res.sendFile(`${__dirname}/success.html`);
-        }
-    };
-    
-    addMember(firstName, lastName, email);
+  addMember(contact);
 });
 
 let port = process.env.PORT;
